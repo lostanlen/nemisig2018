3a5a4c085f8ee6fa3fdf6226b121805bcd9460bf
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2018-01-21 11:28:01 -0500

    update generate_01-sbatch

--- sbatch/generate_01-sbatch.py
+++ sbatch/generate_01-sbatch.py
@@ -6,11 +6,13 @@ import localmodule
 
 
 # Define constants.
-script_name = "046_train-pcen-add-convnet.py"
-script_path = os.path.join("..", "..", "..", "src", script_name)
-units = localmodule.get_units()
-n_trials = 10
-aug_kinds = ["all", "all-but-noise", "none"]
+data_dir = localmodule.get_data_dir()
+dataset_name = localmodule.get_dataset_name()
+composers = localmodule.get_composers()
+kern_name = "_".join([dataset_name, "kern"])
+kern_dir = os.path.join(data_dir, kern_name)
+script_name = "01_eigenprogression_transform.py"
+script_path = os.path.join("..", "src", script_name)
 
 
 # Create folders.
@@ -20,43 +22,39 @@ slurm_dir = os.path.join(script_name[:-3], "slurm")
 os.makedirs(slurm_dir, exist_ok=True)
 
 
-# Loop over augmentations.
-for aug_kind_str in aug_kinds:
+# Loop over composers.
+for composer_str in composers:
 
-    # Loop over recording units.
-    for unit_str in units:
+    composer_dir = os.path.join(kern_dir, composer_str)
+    pieces = os.listdir(composer_dir)
 
-        # Loop over trials.
-        for trial_id in range(n_trials):
+    # Loop over pieces.
+    for piece_name in pieces:
 
-            # Define file path.
-            trial_str = "trial-" + str(trial_id)
-            job_name = "_".join([
-                script_name[:3], "aug-" + aug_kind_str, unit_str, trial_str])
-            file_name = job_name + ".sbatch"
-            file_path = os.path.join(sbatch_dir, file_name)
+        piece_str = piece_name[:-4]
+        job_name = "_".join([
+            script_name[:2], composer_str, piece_str])
+        file_name = job_name + ".sbatch"
+        file_path = os.path.join(sbatch_dir, file_name)
 
-            # Define script.
-            script_list = [script_path, aug_kind_str, unit_str, str(trial_id)]
-            script_path_with_args = " ".join(script_list)
+        # Define script.
+        script_list = [composer_str, piece_str]
+        script_path_with_args = " ".join(script_list)
 
-            # Open file.
-            with open(file_path, "w") as f:
-                f.write("#!/bin/bash\n")
-                f.write("\n")
-                f.write("#BATCH --job-name=" + job_name + "\n")
-                f.write("#SBATCH --nodes=1\n")
-                f.write("#SBATCH --tasks-per-node=1\n")
-                f.write("#SBATCH --cpus-per-task=1\n")
-                if aug_kind_str == "all":
-                    f.write("#SBATCH --time=12:00:00\n")
-                else:
-                    f.write("#SBATCH --time=6:00:00\n")
-                f.write("#SBATCH --mem=8GB\n")
-                f.write("#SBATCH --output=../slurm/slurm_" + job_name + "_%j.out\n")
-                f.write("\n")
-                f.write("module purge\n")
-                f.write("\n")
-                f.write("# The first argument is the name of the recording unit.\n")
-                f.write("# The second argument is the duration of background.\n")
-                f.write("python " + script_path_with_args)
+        # Open file.
+        with open(file_path, "w") as f:
+            f.write("#!/bin/bash\n")
+            f.write("\n")
+            f.write("#BATCH --job-name=" + job_name + "\n")
+            f.write("#SBATCH --nodes=1\n")
+            f.write("#SBATCH --tasks-per-node=1\n")
+            f.write("#SBATCH --cpus-per-task=1\n")
+            f.write("#SBATCH --time=2:00:00\n")
+            f.write("#SBATCH --mem=16GB\n")
+            f.write("#SBATCH --output=../slurm/slurm_" + job_name + "_%j.out\n")
+            f.write("\n")
+            f.write("module purge\n")
+            f.write("\n")
+            f.write("# The first argument is the name of the composer.\n")
+            f.write("# The second argument is the name of the piece.\n")
+            f.write("python " + script_path_with_args)
