143fa5d14da53c32ce00575c0e8e414347775b18
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2018-01-27 13:12:03 -0500

    up

---
+++ julia/eigentriads.jl
@@ -0,0 +1,23 @@
+X = zeros(Int, 12, 12*11*10)
+
+n = 1
+for p1 in 1:12
+    for p2 in 1:12
+        if p2 != p1
+            for p3 in 1:12
+                if p3 != p1 && p3 != p2
+                    X[p1, n] = 1
+                    X[p2, n] = 1
+                    X[p3, n] = 1
+                    n = n + 1
+                end
+            end
+        end
+    end
+end
+
+Cmaj = [1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0]
+X_fourier = abs.(fft(X, 1))
+Cmaj_fourier = abs.(fft(Cmaj, 1))
+Cmaj_fourier = repmat(Cmaj_fourier, 1, 12*11*10)
+dists_fourier = norm(X_fourier - Cmaj_fourier, 1)
