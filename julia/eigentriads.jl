d0dda9df12eeb6f47f06754c62a37cdd2525531f
Vincent Lostanlen <vincent.lostanlen@gmail.com>
2018-01-28 11:17:11 -0500

    up

--- julia/eigentriads.jl
+++ julia/eigentriads.jl
@@ -20,4 +20,5 @@ Cmaj = [1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0]
 X_fourier = abs.(fft(X, 1))
 Cmaj_fourier = abs.(fft(Cmaj, 1))
 Cmaj_fourier = repmat(Cmaj_fourier, 1, 12*11*10)
-dists_fourier = norm(X_fourier - Cmaj_fourier, 1)
+dists_fourier = sum((X_fourier - Cmaj_fourier).*(X_fourier - Cmaj_fourier), 1)
+sorted_dists_fourier, sorting_indices_fourier = sort(vec(dists_fourier))
